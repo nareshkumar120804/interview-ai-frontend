011913d348b71ab99c2e059516d566173db640bb
nareshkumar120804 <nareshkumar120804@gmail.com>
2026-06-16 02:25:56 +0530

    Update API base URL to production endpoint

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://interview-ai-backend-2-zvpg.onrender.com",
   withCredentials: true,
 });
 
